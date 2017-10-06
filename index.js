4ed59dc43685149775a69d1f752399a87f4e918f
Damian Guillory <guillorywdamian@gmail.com>
2017-10-06 01:42:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-    var object = { object, [key]: value}
-    return object
+    return object = { object, [key]: value}
 }
